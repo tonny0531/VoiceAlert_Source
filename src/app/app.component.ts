f16df991a0e1d24148d6d3b189020b01e3d5ad06
Jeff <tonny0531@hotmail.com>
2020-10-31 17:46:41 +0800

    Alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  audio = new Audio();
   title = 'alert-voice';
+  sub: Subscription;
+  source = interval(3000);
+
+  ngOnInit(): void {
+    this.audio.src = 'assets/babubabu.mp3';
+    this.audio.load();
+  }
+
+  play(): void {
+    this.audio.play();
+    this.sub = this.source.subscribe(v => {
+      console.log('log');
+      this.audio.play();
+    });
+  }
+
+  stop(): void {
+      console.log('stop');
+      if (this.sub){
+        this.sub.unsubscribe();
+      }
+  }
 }
